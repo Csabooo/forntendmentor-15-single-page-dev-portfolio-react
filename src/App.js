6648177e79ab0a6bc27a9f96d82d9d492f50e8f0
Müller Csaba <csaba.muller@hotmail.com>
2023-08-09 21:23:11 +0200

    Readme.md modified

--- src/App.js
+++ src/App.js
@@ -2,18 +2,19 @@ import Header from "./components/Header";
 import Main from "./components/Main";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
+import Attribution from "./components/Attribution";
 
 function App() {
   return (
     <div>
       <div className="2xl:container text-center mx-auto pb-10 px-5 md:pb-20">
-        <div className="md:max-w-[768px] lg:max-w-[1110px] mx-auto">
+        <div className="mx-auto md:max-w-[768px] lg:max-w-[1110px]">
           <Header />
           <Main />
           <Projects />
         </div>
       </div>
-      <div className="text-center ">
+      <div className="text-center">
         <Contact />
       </div>
     </div>
